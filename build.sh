c49e101f1ad5f930cb2bed23e3e0c2ed2773d6e2
Caleb Woodbine <calebwoodbine.public@gmail.com>
2023-11-16 09:03:53 +1300

    feat: use raw image build
    build image in raw format

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-
-git clone --branch bifrost-image --depth 1 https://github.com/achilleas-k/images.git
+git clone --branch raw-bifrost-image --depth 1 https://github.com/BobyMCbobs/osbuild-images.git ./images
 cd images
 go build ./cmd/osbuild-deploy-container
